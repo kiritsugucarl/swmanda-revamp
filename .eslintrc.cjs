3b4b4d143bcf23226455734711b6a44879a6dc3c
Kiritsugucarl <bluesniper017@gmail.com>
2023-07-12 15:34:25 +0800

    add: utils

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,3 @@
-/* eslint-env node */
-
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
@@ -8,13 +6,37 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
+  overrides: [
+    {
+        files: ['**/*.cjs'],
+        env: {
+            node: true
+        }
+    }
+  ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    'react-refresh/only-export-components': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-props-no-multi-spaces': 'error',
+    'no-plusplus': [2, { allowForLoopAfterthoughts: true }],
+    'react/function-component-definition': [
+        'error',
+        {
+            namedComponents: ['function-declaration', 'arrow-function'],
+            unnamedComponents: 'arrow-function'
+        }
     ],
-  },
+    'prettier/prettier': [
+        'error',
+        {
+            singleQuote: true,
+            endOfLine: 'off'
+        }
+    ]
+}
 }
--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ function App() {
 
   return (
     <>
-      
+      <p>Paragraph</p>
+      <h1>Big Header</h1>
+      <h2>Sub Title</h2>
+      <button className="btn-1">button</button>
+      <a>Link</a>
     </>
   )
 }
